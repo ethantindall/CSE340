fdc49285bcaafe792a3cdf4daea838df114c2d6e
roguejuan44 <50275473+roguejuan44@users.noreply.github.com>
2020-09-23 13:19:48 -0600

    motors

---
+++ CSE340/phpmotors/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Motors Homepage</title>
+
+</head>
+<body>
+    <header>
+    </header>
+    <main>
+        <h1>Content Title Here</h1>
+    </main>
+    <footer>
+        <hr>
+        <p>&copy; PHP Motors, All rights reserved.</p>
+        <p>All images are believed to be "Fair Use". Please notify the author if any are not and they will be removed.</p>
+        <span id="lastUpdated"></span>
+    </footer>
+
+
+</body>
+</html>
